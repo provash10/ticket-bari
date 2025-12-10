997780f0368fafe25a35a3ad98108d3a363fe2e9
Provash <provash20cb@gmail.com>
2025-12-10 15:19:47 +0500

    Private Route

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import useAuth from '../Hooks/useAuth';
+import { Navigate } from 'react-router';
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useAuth();
+
+    if(loading){
+        return <div>
+            <span className="loading loading-spinner text-warning"></span>
+        </div>
+    }
+    if(!user){
+        return <Navigate to='/login'></Navigate>
+    }
+
+    return children;
+};
+
+export default PrivateRoute;
